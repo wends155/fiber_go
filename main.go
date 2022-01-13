be46980bf4bc16d5e06ae5e46c274066c96d058a
wends155 <saliganw@gmail.com>
2022-01-13 10:25:36 +0800

    test deploy

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, World! again!")
 	})
 	app.Listen(":3000")
 }
