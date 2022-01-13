b037d520ed697dea15d1ee66602a7f715c0b0f55
wends155 <saliganw@gmail.com>
2022-01-13 10:34:28 +0800

    add new route

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+func handler(c *fiber.Ctx) error {
+	return c.SendString("hello from another func")
+}
+
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World! again!")
 	})
+	app.Get("/s", handler)
 	app.Listen(":3000")
 }
