869e76c95eba5062b96ab56fba7c0b7501a318da
wends155 <saliganw@gmail.com>
2022-01-13 16:34:10 +0800

    under contrs

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,7 +3,7 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func Main() fiber.Handler {
-	greet := "Welcome to sapangpera.online. Site is under construction. "
+	greet := "Welcome to sapangpera.online. Site is under construction. Ate Dhories ginagawa pa ah, hehe. testing lang to na pangalan."
 	return func(c *fiber.Ctx) error {
 		return c.SendString(greet)
 	}
