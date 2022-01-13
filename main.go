11f1f0554dd9d63b7a22d419dc30d228ac758c6a
wends155 <saliganw@gmail.com>
2022-01-13 12:45:11 +0800

    add task

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+)
 
 func handler(c *fiber.Ctx) error {
-	return c.SendString("hello from another func")
+	return c.JSON(&fiber.Map{"message": "hello", "status": "OK"})
 }
 
 func main() {
 	app := fiber.New()
+	app.Use(compress.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World! again!")
 	})
